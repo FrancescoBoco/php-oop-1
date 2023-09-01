59455a4f1532b50f83a1fe1626cac81fe10581c7
FrancescoBoco <francesco.bocoux@gmail.com>
2023-09-01 17:24:02 +0200

    ADD script

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+class Movie
+{
+    public $director;
+    public $name;
+    public $year;
+    public $language;
+
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+    function get_name()
+    {
+        return $this->name;
+    }
+};
+
+
+
+$movie = new Movie();
+$movie->$name = 'Fight Club'
+echo $movie->get_name(name);
+?>
